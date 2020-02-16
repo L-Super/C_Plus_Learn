642976895323489bdeddab696cdf608d5638c3b2
L-Super <40905056+L-Super@users.noreply.github.com>
2020-02-16 11:53:00 +0800

    Add files via upload

---
+++ 字符串输出.c
@@ -0,0 +1,37 @@
+//字符串输出，自定义输入输出函数
+#include<stdio.h>
+ /*int main()
+ {
+ 	char s[]="side a";
+ 	char dont[]={'w','o','w','!'};
+ 	char d[]="side b";
+ 	puts(dont);	//不是一个字符串，缺少空字符 
+ 	return 0;
+ }*/ 
+void put1(const char*);
+int put2(const char*);
+int main(void)
+{
+	put1("if I'd as much money");
+	put1("as I could spend,\n");
+	printf("I count %d characters.\n",put2("I never would cry old chairs to mend."));
+	return 0;
+ } 
+ void put1(const char *string)
+ {
+ 	while(*string)	//与*string!='\0'相同 
+ 	putchar(*string++);
+  } 
+  int put2(const char *string)
+  {
+  	int count=0;
+  	while(*string)
+  	{
+  		putchar(*string++);
+  		count++;
+	  }
+	  putchar('\n');
+	  return (count);
+	  /先打印输出字符串，再打印字符数count 
+  }
+
